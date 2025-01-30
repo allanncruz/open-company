ae4c369542e6d82e940c7a7aeb2e754a467ee82a
Allan <allancruz716@gmail.com>
2025-01-30 08:12:13 -0300

    feat: dinamic title page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterCompanyComponent } from './pages/register-company/register-company.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'solicitar-abertura', component: RegisterCompanyComponent },
+  { path: '', component: HomeComponent, data: { title: 'Pedido de Abertura de empresas' } },
+  { path: 'solicitar-abertura', component: RegisterCompanyComponent, data: { title: 'Solicitar Abertura de empresas' } },
 ];
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  pageTitle: string = '';
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute.firstChild;
+        while (route?.firstChild) {
+          route = route.firstChild;
+        }
+        return route?.snapshot.data['title'] || 'Abertura de empresas';
+      })
+    ).subscribe(title => this.pageTitle = title);
+  }
 }
